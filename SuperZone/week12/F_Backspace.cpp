61995d6c306948634ef7d2020999d5e5182fbc3f
IshtyMahin <ishty119@gmail.com>
2023-12-21 18:19:58 +0600

    week12 21/12/23

---
+++ SuperZone/week12/F_Backspace.cpp
@@ -0,0 +1,58 @@
+#include <bits/stdc++.h>
+using namespace std;
+typedef long long ll;
+#define pb push_back
+#define vi vector<int>
+#define vl vector<ll>
+#define nl '\n'
+#define yes cout << "YES" << nl
+#define no cout << "NO" << nl
+#define error cout << -1 << nl
+#define srt(v) sort((v).begin(), (v).end())
+#define coutv(v) for(auto it:v)cout<<it<<' ';cout<<nl;
+#define cout(v) cout<<v<<nl;
+#define cinv(v) for(auto &it:v)cin>>it;
+#define cin(v) cin>>v;
+#define rvr(v) reverse(srt(v))
+#define all(v) (v).begin(), (v).end()
+#define FAST                           \
+    ios_base ::sync_with_stdio(false); \
+    cin.tie(0);                        \
+    cout.tie(0)
+
+void solve(int q)
+{
+   string s,t;cin>>s>>t;
+   ll n = s.size();
+   ll m = t.size();
+  
+   for(int i=n-1;i>=0;i--){
+       if(s[i]!=t[m-1]){
+         i--;
+       }
+       else{
+         m--;
+       }
+       if(m==0)break;
+   }
+
+   if(m==0){
+    yes;
+   }
+   else{
+    no;
+   }
+
+}
+
+int main()
+{
+    FAST;
+    int t = 1;
+    cin >> t;
+    while (t--)
+    {
+        solve(t);
+    }
+    return 0;
+}
---
+++ SuperZone/week12/H_Madoka_and_Formal_Statement.cpp
@@ -0,0 +1,57 @@
+#include <bits/stdc++.h>
+using namespace std;
+typedef long long ll;
+#define pb push_back
+#define vi vector<int>
+#define vl vector<ll>
+#define nl '\n'
+#define yes cout << "YES" << nl
+#define no cout << "NO" << nl
+#define error cout << -1 << nl
+#define srt(v) sort((v).begin(), (v).end())
+#define coutv(v) for(auto it:v)cout<<it<<' ';cout<<nl;
+#define cout(v) cout<<v<<nl;
+#define cinv(v) for(auto &it:v)cin>>it;
+#define cin(v) cin>>v;
+#define rvr(v) reverse(srt(v))
+#define all(v) (v).begin(), (v).end()
+#define FAST                           \
+    ios_base ::sync_with_stdio(false); \
+    cin.tie(0);                        \
+    cout.tie(0)
+
+void solve(int t)
+{
+    ll n;cin>>n;
+    vl a(n),b(n);
+    for(int i=0;i<n;i++)cin>>a[i];
+    for(int i=0;i<n;i++)cin>>b[i];
+
+    for(int i=0;i<n;i++){
+        if(a[i]==b[i])continue;
+
+        if(a[i]>b[i]){
+            no;
+            return;
+        }
+        else{
+            if(b[i]>b[(i+1)%n]+1){
+              no;
+              return;
+            }
+        }
+    }
+    yes;
+}
+
+int main()
+{
+    FAST;
+    int t = 1;
+    cin >> t;
+    while (t--)
+    {
+        solve(t);
+    }
+    return 0;
+}
